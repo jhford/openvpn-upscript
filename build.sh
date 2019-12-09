213a558e84d401aaa8fd5b7f7315fafadf172576
John Ford <john@johnford.org>
2019-12-09 21:59:21 +0100

    cleanup

--- build.sh
+++ build.sh
@@ -4,7 +4,5 @@ set -e
 version="0.0.1"
 commit="$(git rev-parse HEAD)"
 
-echo $version $commit
-
 go build -ldflags "-s -w -X github.com/jhford/openvpn-up/pkg.Version=$version -X github.com/jhford/openvpn-up/pkg.Commit=$commit" ./cmd/up
 upx --best --brute ./up
